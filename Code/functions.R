e0a3f13130903ed39b8cddbbcb198a1858e0360f
djhocking <djhocking@gmail.com>
2019-03-20 22:27:12 -0400

    moved reconstruction plot code to helper function script to use via source

---
+++ Code/functions.R
@@ -0,0 +1,31 @@
+# reconstruction plot
+plot_recon <- function(mcmc, obs = climate_df, x_mean = x_mean, x_sd = x_sd, valid_yrs = NULL) {
+  xidx_m2 = which(substr(varnames(mcmc),1,2)=="x[")
+  temp_df <- as_tibble(t(as.matrix(mcmc[ , xidx_m2])))
+  temp_df <- temp_df %>% 
+    mutate(year = year)
+  
+  temp_df_long <- temp_df %>% 
+    gather(key = sim, value = temp, -year) %>%
+    dplyr::mutate(temp = temp*x_sd + x_mean)
+  
+  # Validation plot
+  if(!is.null(valid_yrs)) {
+    temp_valid <- temp_df_long %>%
+      dplyr::filter(year %in% valid_yrs) %>%
+      dplyr::mutate(Value = "estimated")
+    
+    climate_valid <- obs %>%
+      dplyr::mutate(Value = "observed") %>%
+      dplyr::rename(temp = value) %>% # x_full) %>%
+      dplyr::filter(year %in% valid_yrs)
+    
+    g <- ggplot(temp_valid, aes(x = year, y = temp))
+    g <- g + geom_fan() + geom_line(data = climate_valid, aes(year, temp), colour = "red") + ylab("Temperature (C)") + xlab("Year") + scale_fill_distiller() + theme_bw()
+  } else {
+    # scaled posterior interval
+    g <- ggplot(temp_df_long, aes(x = year, y = temp))
+    g <- g + geom_fan() + geom_line(data = obs, aes(x = year, y = value), colour="black", size = 0.2) + ylab("Temperature (C)") + xlab("Year") + theme_bw() + scale_fill_distiller() #x_full),
+  }
+  return(g)
+}
